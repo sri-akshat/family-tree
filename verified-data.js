a6e3b80b5e7a785576aa02ce8052dd3ce3255d27
Akshat Srivastava <akshat.srivastava@clear.in>
2026-08-14 00:31:34 +0530

    Expand verified family tree from source review

--- verified-data.js
+++ verified-data.js
@@ -1,80 +1,182 @@
 window.VERIFIED_TREE = {
-  id: 'akshat',
-  name: 'Akshat',
-  relation: 'Self',
-  children: [
+  "id": "akshat",
+  "name": "Akshat",
+  "relation": "Self",
+  "children": [
     {
-      id: 'parents',
-      name: 'Parents',
-      group: true,
-      children: [
-        { id: 'anil', name: 'Anil', relation: 'Father' },
+      "id": "parents",
+      "name": "Parents",
+      "children": [
         {
-          id: 'madhu',
-          name: 'Madhu',
-          relation: 'Mother',
-          children: [
-            { id: 'gayatri', name: 'Gayatri', relation: 'Mother' },
+          "id": "anil",
+          "name": "Anil",
+          "relation": "Father",
+          "children": [
             {
-              id: 'pinky', name: 'Pinky', relation: 'Sister', spouse: 'Hari',
-              children: [{ id: 'sona', name: 'Sona', relation: 'Son' }]
-            },
-            {
-              id: 'amita', name: 'Amita', relation: 'Sister', spouse: 'Rakesh',
-              children: [
-                {
-                  id: 'ketu', name: 'Ketu', relation: 'Son', spouse: 'Divya',
-                  children: [{ id: 'ojas', name: 'Ojas', relation: 'Son' }]
-                }
+              "id": "anil-kusum",
+              "name": "Kusum",
+              "relation": "Sister",
+              "spouse": "Ramapati Ram",
+              "children": [
+                {"id":"anil-kusum-rinki","name":"Rinki","relation":"Child"},
+                {"id":"anil-kusum-mukesh","name":"Mukesh","relation":"Child","spouse":"Divya"}
               ]
             },
+            {"id":"anil-lekha","name":"Lekha","relation":"Sister"},
+            {"id":"anil-ajay","name":"Ajay","relation":"Brother"},
+            {"id":"anil-vijay","name":"Vijay","relation":"Brother"},
             {
-              id: 'sapna', name: 'Sapna', relation: 'Sister', spouse: 'Pradeep',
-              children: [{ id: 'prabhat', name: 'Prabhat', relation: 'Son' }]
-            },
-            {
-              id: 'vandana', name: 'Vandana', relation: 'Sister', spouse: 'Rajeev',
-              children: [
+              "id":"anil-shambhu","name":"Shambhu","relation":"Father","children":[
                 {
-                  id: 'jaanu', name: 'Jaanu', relation: 'Son', spouse: 'Mona',
-                  children: [{ id: 'naksh', name: 'Naksh', relation: 'Son' }]
-                }
-              ]
-            },
-            {
-              id: 'chhamta', name: 'Chhamta', relation: 'Sister', spouse: 'Ashok',
-              children: [
-                {
-                  id: 'kunal', name: 'Kunal', relation: 'Son', spouse: 'Sheela',
-                  children: [
-                    { id: 'neel', name: 'Neel', relation: 'Son' },
-                    { id: 'nova', name: 'Nova', relation: 'Son' },
-                    { id: 'liza', name: 'Liza', relation: 'Daughter' }
+                  "id":"anil-shambhu-barabanki","name":"Barabanki Sister","relation":"Sister","children":[
+                    {"id":"barabanki-rama","name":"Rama","relation":"Daughter","children":[
+                      {"id":"rama-soni","name":"Soni","relation":"Daughter","spouse":"Rajeev","children":[{"id":"soni-sanket","name":"Sanket","relation":"Son"}]},
+                      {"id":"rama-moni","name":"Moni","relation":"Daughter","spouse":"Gappu","children":[{"id":"moni-ojas","name":"Ojas","relation":"Son"},{"id":"moni-pihu","name":"Pihu","relation":"Daughter"}]},
+                      {"id":"rama-pooja","name":"Pooja","relation":"Daughter"},
+                      {"id":"rama-meenu","name":"Meenu","relation":"Daughter","children":[{"id":"meenu-krishna","name":"Krishna","relation":"Son"}]}
+                    ]},
+                    {"id":"barabanki-ashok","name":"Ashok","relation":"Son","spouse":"Mithlesh","children":[
+                      {"id":"ashok-monika","name":"Monika","relation":"Daughter","spouse":"Amit","children":[{"id":"monika-aarna","name":"Aarna","relation":"Daughter"}]},
+                      {"id":"ashok-amit","name":"Amit","relation":"Son","spouse":"Shilpi","children":[{"id":"amit-atharva","name":"Atharva","relation":"Son"},{"id":"amit-vivan","name":"Vivan","relation":"Son"}]},
+                      {"id":"ashok-manisha","name":"Manisha","relation":"Daughter","spouse":"Vikas","children":[{"id":"manisha-manvi","name":"Manvi","relation":"Daughter"},{"id":"manisha-vidita","name":"Vidita","relation":"Daughter"}]}
+                    ]},
+                    {"id":"barabanki-usha","name":"Usha","relation":"Daughter"},
+                    {"id":"barabanki-devesh","name":"Devesh","relation":"Son"},
+                    {"id":"barabanki-sudha","name":"Sudha","relation":"Daughter"},
+                    {"id":"barabanki-piyush","name":"Piyush","relation":"Son"},
+                    {"id":"barabanki-rajesh","name":"Rajesh","relation":"Son"}
                   ]
                 },
-                { id: 'ketan', name: 'Ketan', relation: 'Son' }
+                {"id":"anil-shambhu-maheshwari","name":"Maheshwari","relation":"Wife 3"},
+                {"id":"anil-shambhu-dinesh","name":"Dinesh","relation":"Son"},
+                {"id":"anil-shambhu-umesh","name":"Umesh","relation":"Son"},
+                {"id":"anil-prabhu-dayal","name":"Prabhu Dayal","relation":"Father","children":[
+                  {"id":"anil-ganesh-prasad","name":"Ganesh Prasad","relation":"Father","children":[
+                    {"id":"anil-shiv-sahay","name":"Shiv Sahay","relation":"Father","children":[{"id":"anil-bal-govind","name":"Bal Govind","relation":"Son"}]},
+                    {"id":"anil-raghuvar-dayal","name":"Raghuvar Dayal","relation":"Son","children":[
+                      {"id":"anil-jagdamba-prasad","name":"Jagdamba Prasad","relation":"Son","children":[
+                        {"id":"anil-kamesh","name":"Kamesh","relation":"Son","children":[
+                          {"id":"anil-gauri-prasad","name":"Gauri Prasad","relation":"Son","children":[
+                            {"id":"anil-shyamji-lal","name":"Shyamji Lal","relation":"Son","children":[
+                              {"id":"anil-madan-kishore","name":"Madan Kishore","relation":"Son"},
+                              {"id":"anil-ratan-kishore","name":"Ratan Kishore (Lullar)","relation":"Son"},
+                              {"id":"anil-kamal-kishore","name":"Kamal Kishore","relation":"Son"},
+                              {"id":"anil-kaushal-kishore","name":"Kaushal Kishore","relation":"Son"},
+                              {"id":"anil-raj-kishore","name":"Raj Kishore","relation":"Son"},
+                              {"id":"anil-sanjay","name":"Sanjay","relation":"Son"}
+                            ]},
+                            {"id":"anil-ramji-lal","name":"Ramji Lal","relation":"Son"},
+                            {"id":"anil-balramji","name":"Balramji","relation":"Son"}
+                          ]},
+                          {"id":"anil-ram-kishore","name":"Ram Kishore","relation":"Son"}
+                        ]},
+                        {"id":"anil-uma-shankar","name":"Uma Shankar","relation":"Son"}
+                      ]},
+                      {"id":"anil-chhote-lal","name":"Chhote Lal","relation":"Son"}
+                    ]},
+                    {"id":"anil-girija-dayal","name":"Girija Dayal","relation":"Son","children":[
+                      {"id":"anil-janki-saran","name":"Janki Saran","relation":"Son","children":[
+                        {"id":"anil-kamlesh","name":"Kamlesh","relation":"Son","children":[{"id":"anil-arunesh","name":"Arunesh","relation":"Son"}]},
+                        {"id":"anil-umesh-2","name":"Umesh","relation":"Son","children":[{"id":"anil-babbu","name":"Babbu","relation":"Son"}]}
+                      ]},
+                      {"id":"anil-kali-saran","name":"Kali Saran","relation":"Son","children":[{"id":"anil-suresh-kumar","name":"Suresh Kumar","relation":"Son"}]}
+                    ]}
+                  ]}
+                ]}
               ]
             }
+          ]
+        },
+        {
+          "id":"madhu","name":"Madhu","relation":"Mother","children":[
+            {"id":"gayatri","name":"Gayatri","relation":"Mother","children":[
+              {"id":"gayatri-suman","name":"Suman","relation":"Sister","spouse":"Chunnu","children":[
+                {"id":"gayatri-suman-nikki","name":"Nikki","relation":"Son","spouse":"Neha"},
+                {"id":"gayatri-suman-mitthu","name":"Mitthu","relation":"Son"},
+                {"id":"chunnu-munnu","name":"Munnu","relation":"Brother of Chunnu","spouse":"Anju","children":[{"id":"munnu-mona","name":"Mona","relation":"Daughter"}]}
+              ]},
+              {"id":"gayatri-rama","name":"Rama","relation":"Sister","spouse":"Jagat","children":[
+                {"id":"rama-jyoti","name":"Jyoti","relation":"Sister","children":[{"id":"jyoti-sameer","name":"Sameer","relation":"Child"}]},
+                {"id":"rama-ruchi","name":"Ruchi","relation":"Sister"},
+                {"id":"rama-rachna","name":"Rachna","relation":"Sister","children":[{"id":"rachna-sudesh","name":"Sudesh","relation":"Child"}]},
+                {"id":"rama-raju","name":"Raju","relation":"Son","children":[{"id":"raju-manav","name":"Manav","relation":"Child"},{"id":"raju-geeta","name":"Geeta","relation":"Child"},{"id":"raju-sahiba","name":"Sahiba","relation":"Child"}]},
+                {"id":"rama-rajat","name":"Rajat","relation":"Son","children":[{"id":"rajat-sapna","name":"Sapna","relation":"Child"}]}
+              ]},
+              {"id":"gayatri-shyam","name":"Shyam","relation":"Sister","children":[
+                {"id":"shyam-satguru","name":"Satguru","relation":"Son","children":[{"id":"satguru-anju","name":"Anju","relation":"Child"}]},
+                {"id":"shyam-swami","name":"Swami","relation":"Son"},
+                {"id":"shyam-dayal","name":"Dayal","relation":"Son"},
+                {"id":"shyam-kripal","name":"Kripal","relation":"Son"},
+                {"id":"shyam-meera","name":"Meera","relation":"Daughter"}
+              ]}
+            ]},
+            {"id":"pinky","name":"Pinky","relation":"Sister","spouse":"Hari","children":[{"id":"sona","name":"Sona","relation":"Son"}]},
+            {"id":"amita","name":"Amita","relation":"Sister","spouse":"Rakesh","children":[{"id":"ketu","name":"Ketu","relation":"Son","spouse":"Divya","children":[{"id":"ojas","name":"Ojas","relation":"Son"}]}]},
+            {"id":"sapna","name":"Sapna","relation":"Sister","spouse":"Pradeep","children":[{"id":"prabhat","name":"Prabhat","relation":"Son"}]},
+            {"id":"vandana","name":"Vandana","relation":"Sister","spouse":"Rajeev","children":[{"id":"jaanu","name":"Jaanu","relation":"Son","spouse":"Mona","children":[{"id":"naksh","name":"Naksh","relation":"Son"}]}]},
+            {"id":"chhamta","name":"Chhamta","relation":"Sister","spouse":"Ashok","children":[
+              {"id":"kunal","name":"Kunal","relation":"Son","spouse":"Sheela","children":[{"id":"neel","name":"Neel","relation":"Son"},{"id":"nova","name":"Nova","relation":"Son"},{"id":"liza","name":"Liza","relation":"Daughter"}]},
+              {"id":"ketan","name":"Ketan","relation":"Son"}
+            ]}
           ]
         }
       ]
     },
     {
-      id: 'ankur', name: 'Ankur', relation: 'Brother', spouse: 'Abha',
-      children: [
-        { id: 'arnav', name: 'Arnav', relation: 'Son' },
-        { id: 'ananya', name: 'Ananya', relation: 'Daughter' }
+      "id":"ankur","name":"Ankur","relation":"Brother","children":[
+        {"id":"abha","name":"Abha","relation":"Wife","children":[
+          {"id":"abha-vidyavati","name":"Vidyavati","relation":"Mother"},
+          {"id":"abha-vibha","name":"Vibha","relation":"Sister","spouse":"Gaurav","children":[{"id":"abha-vibha-pihu","name":"Pihu","relation":"Daughter"},{"id":"abha-vibha-kuhu","name":"Kuhu","relation":"Daughter"}]},
+          {"id":"abha-abhay","name":"Abhay","relation":"Brother","spouse":"Renu","children":[{"id":"abha-abhay-jiya","name":"Jiya","relation":"Daughter"}]},
+          {"id":"abha-pratibha","name":"Pratibha","relation":"Sister","spouse":"Rajesh","children":[{"id":"abha-pratibha-koko","name":"Koko","relation":"Son"},{"id":"abha-pratibha-cookie","name":"Cookie","relation":"Daughter"}]},
+          {"id":"abha-fathers-brother","name":"Father's brother","relation":"Uncle (name not recorded)","children":[{"id":"abha-dolly","name":"Dolly","relation":"Daughter"},{"id":"abha-rishabh","name":"Rishabh","relation":"Son"}]}
+        ]},
+        {"id":"arnav","name":"Arnav","relation":"Son"},
+        {"id":"ananya","name":"Ananya","relation":"Daughter"}
       ]
     },
     {
-      id: 'garima', name: 'Garima', relation: 'Wife',
-      children: [
-        { id: 'shyam', name: 'Shyam', relation: 'Father' },
-        { id: 'meera', name: 'Meera', relation: 'Mother' },
-        { id: 'prerna', name: 'Prerna', relation: 'Sister' },
-        { id: 'shashwat', name: 'Shashwat', relation: 'Brother' }
+      "id":"garima","name":"Garima","relation":"Wife","children":[
+        {"id":"garima-shyam","name":"Shyam","relation":"Father","children":[
+          {"id":"shyam-usha","name":"Usha","relation":"Sister","spouse":"Ramakant","children":[{"id":"usha-sriprakash","name":"Sriprakash","relation":"Son"},{"id":"usha-narayan","name":"Narayan","relation":"Son"},{"id":"usha-mala","name":"Mala","relation":"Daughter"},{"id":"usha-nanhi","name":"Nanhi","relation":"Daughter"}]},
+          {"id":"shyam-sudha","name":"Sudha","relation":"Sister","spouse":"Devendra","children":[{"id":"sudha-neetu","name":"Neetu","relation":"Daughter","spouse":"Atul"},{"id":"sudha-puneet","name":"Puneet","relation":"Son"},{"id":"sudha-neelam","name":"Neelam","relation":"Daughter"}]},
+          {"id":"shyam-kaushal","name":"Kaushal","relation":"Brother","spouse":"Shubha","children":[
+            {"id":"kaushal-abhishek","name":"Abhishek","relation":"Son","spouse":"Kalpana","children":[{"id":"abhishek-harshita","name":"Harshita","relation":"Daughter"},{"id":"abhishek-shivangi","name":"Shivangi","relation":"Daughter"}]},
+            {"id":"kaushal-prachi","name":"Prachi","relation":"Daughter","spouse":"Saurabh","children":[{"id":"prachi-vishrut","name":"Vishrut","relation":"Son"}]},
+            {"id":"kaushal-sachin","name":"Sachin","relation":"Son","spouse":"Anjali","children":[{"id":"sachin-himani","name":"Himani","relation":"Daughter"}]}
+          ]},
+          {"id":"shyam-sandhya","name":"Sandhya","relation":"Sister","spouse":"Ravendra","children":[{"id":"sandhya-ashwini","name":"Ashwini","relation":"Son"},{"id":"sandhya-akansha","name":"Akansha","relation":"Daughter"}]},
+          {"id":"shyam-prabhudayal","name":"Prabhudayal","relation":"Father","children":[
+            {"id":"prabhudayal-chandravati","name":"Chandravati","relation":"Sister","spouse":"Srinivas","children":[
+              {"id":"chandravati-ramanivas","name":"Ramanivas","relation":"Son","spouse":"Durgesh"},
+              {"id":"chandravati-uma","name":"Uma","relation":"Daughter","spouse":"Ishwar","children":[{"id":"uma-kushi","name":"Kushi","relation":"Son"},{"id":"uma-lovi","name":"Lovi","relation":"Son"}]},
+              {"id":"chandravati-padma","name":"Padma","relation":"Daughter","spouse":"Laalta Prasad"}
+            ]},
+            {"id":"prabhudayal-phoolmati","name":"Phoolmati","relation":"Sister"},
+            {"id":"prabhudayal-uma","name":"Uma","relation":"Sister"},
+            {"id":"prabhudayal-srimati","name":"Srimati","relation":"Sister"},
+            {"id":"prabhudayal-shanti","name":"Shanti","relation":"Sister"},
+            {"id":"prabhudayal-kalika","name":"Kalika Prasad","relation":"Father","children":[{"id":"kalika-padam","name":"Padam Prasad","relation":"Father"}]}
+          ]},
+          {"id":"shyam-vidyavati","name":"Vidyavati","relation":"Mother","children":[{"id":"vidyavati-ramnarayan","name":"Ramnarayan","relation":"Father"},{"id":"vidyavati-sharmeeli","name":"Sharmeeli","relation":"Mother"},{"id":"vidyavati-sunidhi","name":"Sunidhi","relation":"Sister"}]}
+        ]},
+        {"id":"garima-meera","name":"Meera","relation":"Mother","children":[
+          {"id":"meera-suman","name":"Suman","relation":"Sister","spouse":"Kamal","children":[
+            {"id":"suman-rachna","name":"Rachna","relation":"Daughter","spouse":"Saharsh","children":[{"id":"rachna-bhavi","name":"Bhavi","relation":"Son"},{"id":"rachna-chavi","name":"Chavi","relation":"Daughter"}]},
+            {"id":"suman-divya","name":"Divya","relation":"Daughter","spouse":"Tribhuvan","children":[{"id":"divya-tisya","name":"Tisya","relation":"Daughter"}]}
+          ]},
+          {"id":"meera-sadhna","name":"Sadhna","relation":"Sister","children":[{"id":"sadhna-bittu","name":"Bittu","relation":"Child"},{"id":"sadhna-manjari","name":"Manjari","relation":"Child","spouse":"Manish"}]},
+          {"id":"meera-ranjana","name":"Ranjana","relation":"Sister","children":[
+            {"id":"ranjana-ashish","name":"Ashish","relation":"Child","spouse":"Neha","children":[{"id":"ashish-anay","name":"Anay","relation":"Son"},{"id":"ashish-aanya","name":"Aanya","relation":"Daughter"}]},
+            {"id":"ranjana-meenakshi","name":"Meenakshi","relation":"Child","spouse":"Vivek","children":[{"id":"meenakshi-tanay","name":"Tanay","relation":"Son"},{"id":"meenakshi-tanya","name":"Tanya","relation":"Daughter"}]}
+          ]},
+          {"id":"meera-ajay","name":"Ajay","relation":"Brother","spouse":"Uma","children":[{"id":"ajay-anuj","name":"Anuj","relation":"Child","spouse":"Sakshi"},{"id":"ajay-ankit","name":"Ankit","relation":"Child","spouse":"Kanchan","children":[{"id":"ankit-anika","name":"Anika","relation":"Daughter"}]}]},
+          {"id":"meera-vijay","name":"Vijay","relation":"Brother","spouse":"Praveen","children":[{"id":"vijay-manuj","name":"Manuj","relation":"Child"},{"id":"vijay-honey","name":"Honey","relation":"Child","spouse":"Shijil"}]}
+        ]},
+        {"id":"garima-prerna","name":"Prerna","relation":"Sister","spouse":"Vikas","children":[{"id":"prerna-vishrut","name":"Vishrut","relation":"Son"}]},
+        {"id":"garima-shashwat","name":"Shashwat","relation":"Brother"}
       ]
     },
-    { id: 'preyaan', name: 'Preyaan', relation: 'Son' }
+    {"id":"preyaan","name":"Preyaan","relation":"Son"}
   ]
 };
